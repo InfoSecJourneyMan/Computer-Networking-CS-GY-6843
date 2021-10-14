576d7c9849a077669ce7c901985851b76140f22f
InfoSecJourneyMan <ajf9552@nyu.edu>
2021-10-14 09:35:43 -0400

    This code is for my HTTP SERVER I am hoping to debug the ascii encode issue.

--- solution.py
+++ solution.py
@@ -1,34 +1,61 @@
-"""Welcome_assignment answers
-Author: Armando Flores Munos
-Date: 10/7/2021
-Really cool assignment posted for the start of the semester programming work
-Input -All eight questions given in the assignment
- Output - The right answer for the specific question """
+#Author: Armando Flores
+#import socket module
+#This programming assignment will teach me about socket programming connecting two nodes to have a client and server relationship
+#In order to terminate program
+from socket import *
+import sys
 
-def welcome_assignment_answers(question):
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
-    elif question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
-        answer = "mTLS"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
-    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
-    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        answer = "42b76fe51778764973077a5a94056724"
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
-    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 5
-    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 4
-    return answer
+def webServer(port=13331):
+    serverSocket = socket(AF_INET, SOCK_STREAM) #AF_NET is for IPV4 and SOCKET_STREAM is used for TCP
+    print("Socket has been created!")
+    my_hostname = gethostname() #This returns the hostname of the current python interpreter is running on
+    ip_add = gethostbyname(my_hostname) #This will take the hostname and acquire the IP address of the host
+    print("This is ipv4 addi", {ip_add})
+    #Prepare Server Socket
+    serverSocket.bind((ip_add,port)) #We will bind this server to a specific port and host ipv4 address
+    serverSocket.listen(2) #The http server will be listening to incoming request
+    #The 2 means that 2 other connections are kept waiting if theres a 3rd it will be dropped
+    print("Socket will be listening to incoming HTTP Request:")
+    while True:
+        #Establish the connection with the client requesting
+        print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept() #connectionSocket is an object that allows me to send information to the connected Client
+        print("Got Connection from", addr)
+        try:
+            try:
+                message = connectionSocket.recv(1024).decode('utf-8') # this will allow me to receive the message that client
+                print(message)
+                namefile = message.split()[1] #it will take the file name of the request
+                ch = '/'
+                filename = namefile.lstrip(ch)
+                print(filename)
+                f = open(filename,'r')
+                outputdata = f.read()
+                print(outputdata)
+                print("Client is requesting file: ",{outputdata})
 
-#Complete all the questions
+                #Send one HTTP header line into socket.
+                successful_request = 'HTTP/1.1 200 OK' # this is a http status code for successful request
+                connectionSocket.send(successful_request.encode('utf-8')) # the string will be converted to bytes before sent
+
+                #Send the content of the requested file to the client
+
+                for i in range(0, len(outputdata)):
+                    connectionSocket.send(outputdata[i].encode('utf-8'))
+
+                connectionSocket.send("\r\n".encode('utf-8'))
+                connectionSocket.close()
+            except IOError:
+                #Send response message for file not found (404)
+                failed_request = 'HTTP/1.1 404 Not Found' # this is the failed to find the requested hmtl document message
+                connectionSocket.send(failed_request.encode('utf-8')) #Sending the header to the connection Socket client
+
+                #Close client socket
+                connectionSocket.close()
+        except (ConnectionResetError, BrokenPipeError):
+            pass
+    serverSocket.close()
+    sys.exit() #Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
-
-    #debug_question = "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
-    print(welcome_assignment_answers(debug_question))
+    webServer(13331)
