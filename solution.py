6bd3cb0844d6f8ca4cc11108e0bbaff053ee119d
InfoSecJourneyMan <ajf9552@nyu.edu>
2021-10-14 10:10:31 -0400

    I hope I have fixed the encoding. I might just send the html in a variable.

--- solution.py
+++ solution.py
@@ -7,43 +7,43 @@ import sys
 
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM) #AF_NET is for IPV4 and SOCKET_STREAM is used for TCP
-    print("Socket has been created!")
-    my_hostname = gethostname() #This returns the hostname of the current python interpreter is running on
-    ip_add = gethostbyname(my_hostname) #This will take the hostname and acquire the IP address of the host
-    print("This is ipv4 addi", {ip_add})
+    #print("Socket has been created!")
+    #my_hostname = gethostname() #This returns the hostname of the current python interpreter is running on
+    #ip_add = gethostbyname(my_hostname) #This will take the hostname and acquire the IP address of the host
+    #print("This is ipv4 addi", {ip_add})
     #Prepare Server Socket
-    serverSocket.bind((ip_add,port)) #We will bind this server to a specific port and host ipv4 address
+    serverSocket.bind(('',port)) #We will bind this server to a specific port and host ipv4 address
     serverSocket.listen(2) #The http server will be listening to incoming request
     #The 2 means that 2 other connections are kept waiting if theres a 3rd it will be dropped
-    print("Socket will be listening to incoming HTTP Request:")
+    #print("Socket will be listening to incoming HTTP Request:")
     while True:
         #Establish the connection with the client requesting
-        print('Ready to serve...')
+        #print('Ready to serve...')
         connectionSocket, addr = serverSocket.accept() #connectionSocket is an object that allows me to send information to the connected Client
-        print("Got Connection from", addr)
+        #print("Got Connection from", addr)
         try:
             try:
                 message = connectionSocket.recv(1024).decode('utf-8') # this will allow me to receive the message that client
-                print(message)
+                #print(message)
                 namefile = message.split()[1] #it will take the file name of the request
                 ch = '/'
                 filename = namefile.lstrip(ch)
-                print(filename)
+                #print(filename)
                 f = open(filename,'r')
                 outputdata = f.read()
-                print(outputdata)
-                print("Client is requesting file: ",{outputdata})
+                #print(outputdata)
+                #print("Client is requesting file: ",{outputdata})
 
                 #Send one HTTP header line into socket.
                 successful_request = 'HTTP/1.1 200 OK' # this is a http status code for successful request
-                connectionSocket.send(successful_request.encode('utf-8')) # the string will be converted to bytes before sent
+                connectionSocket.send(successful_request.encode()) # the string will be converted to bytes before sent
 
                 #Send the content of the requested file to the client
 
                 for i in range(0, len(outputdata)):
-                    connectionSocket.send(outputdata[i].encode('utf-8'))
+                    connectionSocket.send(outputdata[i].encode())
 
-                connectionSocket.send("\r\n".encode('utf-8'))
+                connectionSocket.send("\r\n".encode())
                 connectionSocket.close()
             except IOError:
                 #Send response message for file not found (404)
