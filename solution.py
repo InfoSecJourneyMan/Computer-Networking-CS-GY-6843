18c3e17b79761a48e76547de68d369e9f12d7a6c
InfoSecJourneyMan <ajf9552@nyu.edu>
2021-12-07 16:32:09 -0500

    Followed the code hope it works.

--- solution.py
+++ solution.py
@@ -1,34 +1,77 @@
-"""Welcome_assignment answers
-Author: Armando Flores Munos
-Date: 10/7/2021
-Really cool assignment posted for the start of the semester programming work
-Input -All eight questions given in the assignment
- Output - The right answer for the specific question """
+from socket import *
 
-def welcome_assignment_answers(question):
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
-    elif question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
-        answer = "mTLS"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
-    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
-    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        answer = "42b76fe51778764973077a5a94056724"
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
-    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 5
-    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 4
-    return answer
 
-#Complete all the questions
+def smtp_client(port=1025, mailserver='127.0.0.1'):
+    msg = "\r\n My message"
+    endmsg = "\r\n.\r\n"
 
-if __name__ == "__main__":
+    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
 
-    #debug_question = "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
-    print(welcome_assignment_answers(debug_question))
+    # Create socket called clientSocket and establish a TCP connection with mailserver and port
+
+    # Fill in start
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    clientSocket.bind((mailserver, port))
+    # Fill in end
+
+    recv = clientSocket.recv(1024).decode()
+    print(recv)
+    if recv[:3] != '220':
+        print('220 reply not received from server.')
+
+    # Send HELO command and print server response.
+    heloCommand = 'HELO Alice\r\n'
+    clientSocket.send(heloCommand.encode())
+    recv1 = clientSocket.recv(1024).decode()
+    print(recv1)
+    if recv1[:3] != '250':
+        print('250 reply not received from server.')
+
+    # Send MAIL FROM command and print server response.
+    mail_from = 'MAIL FROM:<ajf9552@nyu.edu>\r\n'
+    clientSocket.send(mail_from.encode())
+    # Fill in end
+
+    # Send RCPT TO command and print server response.
+    # Fill in start
+    rec = 'RCPT TO:<gogo@mail.edu>\r\n'
+    clientSocket.send(rec.encode())
+    recv3 = clientSocket.recv(1024).decode()
+    print(recv3)
+    # Fill in end
+
+    # Send DATA command and print server response.
+    # Fill in start
+    dat = 'DATA'
+    clientSocket.socket(dat.encode())
+    recv4 = clientSocket.recv(1024).decode()
+    print(recv4)
+    # Fill in end
+
+    # Send message data.
+    # Fill in start
+    clientSocket.send(msg.encode())
+    recv5 = clientSocket.recv(1024).decode()
+    print(rec5)
+    # Fill in end
+
+    # Message ends with a single period.
+    # Fill in start
+    clientSocket.send(endmsg.encode())
+    recv6 = clientSocket.recv(1024).decode()
+    print(recv6)
+    # Fill in end
+
+    # Send QUIT command and get server response.
+    # Fill in start
+    end_of_mail = 'QUIT\r\n'
+    clientSocket.send(end_of_mail.encode())
+    recv7 = clientSocket.recv(1024).decode()
+    print(recv7)
+    # Fill in end
+    clientSocket.close()
+
+
+if __name__ == '__main__':
+    smtp_client(1025, '127.0.0.1')
